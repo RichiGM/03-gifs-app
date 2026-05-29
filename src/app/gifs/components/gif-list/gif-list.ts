a921a1dc381d9b04847ac1f5c48ac92c00a24eb7
RichiGM <garnicamejiaricardo@gmail.com>
2026-05-29 10:29:52 -0600

    feat: app de gifs completada con consumo de api y localstorage

---
+++ src/app/gifs/components/gif-list/gif-list.ts
@@ -0,0 +1,12 @@
+import { Component, input } from '@angular/core';
+import { GifListItemComponent } from "./gif-list-item/gif-list-item";
+import { Gif } from '../../interfaces/gif.interface';
+
+@Component({
+  selector: 'gif-list',
+  imports: [GifListItemComponent],
+  templateUrl: './gif-list.html',
+})
+export class GifListComponent {
+  gifs = input.required<Gif[]>()
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+    companyName: 'Gifs',
+    companyName2: 'App',
+    companySlogan: "Maneja tus gifs",
+
+    giphyApiKey: 'eJ22B16YBjZ81bL0nlz3ZyYEi28GoxOq',
+    giphyUrl: 'https://api.giphy.com/v1'
+};
